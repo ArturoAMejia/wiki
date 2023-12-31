707fa9e63507c40bf52e74cdcf09ad14e0d549a3
Arturo Mejía <armejia02@gmail.com>
2023-12-30 23:03:00 -0600

    new page, edit page, random page and conversion done

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -4,5 +4,9 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/<str:title>", views.entry, name="entry")
+    path("wiki/<str:title>", views.entry, name="entry"),
+    path("error/", views.error_page, name="error"),
+    path("new-page/", views.new_page, name="new_page"),
+    path("edit-page/<str:title>", views.edit_page, name="edit_page"),
+    path("random-page/", views.random_page, name="random_page")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from random import randrange
 from . import util
+from markdown2 import Markdown
 
 
 def index(request):
@@ -9,12 +10,59 @@ def index(request):
     })
 
 
+def error_page(request):
+    return render(request, 'encyclopedia/404.html')
+
+
 def entry(request, title):
     entry = util.get_entry(title)
 
     if (entry == None):
         return render(request, "encyclopedia/404.html")
-    
-    print(entry)
+    markdowner = Markdown()
 
-    return render(request, "encyclopedia/entry.html", { "name": title})
+    content = markdowner.convert(entry)
+
+    return render(request, "encyclopedia/entry.html", { "name": title, "content": content })
+
+
+def new_page(request):
+    if request.method == 'GET':
+        return render(request, 'encyclopedia/new-page.html')
+    else:
+        title = request.POST['title']
+        content = request.POST['content']
+
+        entries = util.list_entries()
+
+        print(entries)
+
+        if (title in entries):
+            print('---------')
+            return render(request, 'encyclopedia/new-page.html', {"error" : "This encyclopedia already exists."})
+        else:
+            util.save_entry(title, content)
+            return redirect(f"/wiki/{title}")
+        
+
+def edit_page(request, title):
+    if request.method == 'GET':
+        entry = util.get_entry(title)
+        print(entry)
+        return render(request, 'encyclopedia/edit-page.html', {"title": title, "content": entry})
+    else:
+        content = request.POST['content']
+
+        util.save_entry(title, content)
+        return redirect(f"/wiki/{title}")
+    
+
+def random_page(request):
+
+    entries = util.list_entries()
+
+    random_number = randrange(0, entries.__len__())
+
+    entry = util.get_entry(entries[random_number])
+
+    return redirect(f"/wiki/{entries[random_number]}")
